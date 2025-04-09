2a42ce9d97cabd10f6763cb75e6c2f49b9b813f7
sarvamangala-c <sarvamangala123456789@gmail.com>
2025-04-09 14:31:19 +0530

    save

--- Add.java
+++ Add.java
@@ -8,7 +8,10 @@ public static void main(String[] args)
         int a=100;
         int b=200;
         int sum=a+b;
+        int sub=a-b;
         System.out.println("sum is:"+sum);
+        System.out.println("sum is:"+sub);
+        System.out.println("master branch");
     }
     
 }
